f2c3442d5150465bd11b621d84fc71ec784c8d32
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2018-08-04 02:34:16 +0300

    commenting out pages not used yet

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,42 @@
 const path = require('path')
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
-    const { createNodeField } = boundActionCreators
-    if (node.internal.type === `ContentfulTeacher`) {
-      createNodeField({
-        node,
-        name: `slug`,
-        value: 'teachers/'+node.name.replace(/ /g,"_").toLowerCase(),
-      })
-    }
+    // const { createNodeField } = boundActionCreators
+    // if (node.internal.type === `ContentfulTeacher`) {
+    //   createNodeField({
+    //     node,
+    //     name: `slug`,
+    //     value: 'teachers/'+node.name.replace(/ /g,"_").toLowerCase(),
+    //   })
+    // }
   };
 
   exports.createPages = ({ graphql, boundActionCreators }) => {
-    const { createPage } = boundActionCreators
-    return new Promise((resolve, reject) => {
-      graphql(`
-        {
-          allContentfulTeacher {
-            edges {
-              node {
-                fields {
-                  slug
-                }
-              }
-            }
-          }
-        }
-      `).then(result => {
-        result.data.allContentfulTeacher.edges.forEach(({ node }) => {
-          createPage({
-            path: node.fields.slug,
-            component: path.resolve(`./src/templates/teacher.js`),
-            context: {
-              slug: node.fields.slug,
-            },
-          })
-        })
-        resolve()
-      })
-    })
+    // const { createPage } = boundActionCreators
+    // return new Promise((resolve, reject) => {
+    //   graphql(`
+    //     {
+    //       allContentfulTeacher {
+    //         edges {
+    //           node {
+    //             fields {
+    //               slug
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   `).then(result => {
+    //     result.data.allContentfulTeacher.edges.forEach(({ node }) => {
+    //       createPage({
+    //         path: node.fields.slug,
+    //         component: path.resolve(`./src/templates/teacher.js`),
+    //         context: {
+    //           slug: node.fields.slug,
+    //         },
+    //       })
+    //     })
+    //     resolve()
+    //   })
+    // })
   };
--- src/pages/teachers.js
+++ src/pages/teachers.js
@@ -1,42 +1,42 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Container from "../components/container"
-import Img from 'gatsby-image';
+// import React from 'react'
+// import Link from 'gatsby-link'
+// import Container from "../components/container"
+// import Img from 'gatsby-image';
 
-const TeachersPage = ({data}) => {
-  return <Container backdrop={data.backdrop.sizes}>
-    {data.teachers.edges.map(({node}) => 
-    <div>
+// const TeachersPage = ({data}) => {
+//   return <Container backdrop={data.backdrop.sizes}>
+//     {data.teachers.edges.map(({node}) => 
+//     <div>
       
-      <p><Link to={"/teachers/"+node.name.replace(/ /g,"_").toLowerCase()}>{node.name}</Link></p>  
-      <Img sizes={node.profilePicture.sizes} />
-      </div>
-    )}
-  </Container>
-}
+//       <p><Link to={"/teachers/"+node.name.replace(/ /g,"_").toLowerCase()}>{node.name}</Link></p>  
+//       <Img sizes={node.profilePicture.sizes} />
+//       </div>
+//     )}
+//   </Container>
+// }
 
-export default TeachersPage
+// export default TeachersPage
 
-export const query = graphql`
-  query TeachersPageQuery {
-    backdrop: contentfulAsset(title: {eq: "rkmacharya_1"}) {
-      sizes(quality: 100 maxWidth: 5000) {
-       ...GatsbyContentfulSizes_withWebp
-      }
-    }
+// export const query = graphql`
+//   query TeachersPageQuery {
+//     backdrop: contentfulAsset(title: {eq: "rkmacharya_1"}) {
+//       sizes(quality: 100 maxWidth: 5000) {
+//        ...GatsbyContentfulSizes_withWebp
+//       }
+//     }
 
-    teachers: allContentfulTeacher {
-      edges {
-        node {
-          id
-          name
-          profilePicture {
-            sizes(quality: 100) {
-              ...GatsbyContentfulSizes_withWebp
-             }
-          }
-        }
-      }
-    }
-  }
-`
+//     teachers: allContentfulTeacher {
+//       edges {
+//         node {
+//           id
+//           name
+//           profilePicture {
+//             sizes(quality: 100) {
+//               ...GatsbyContentfulSizes_withWebp
+//              }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
