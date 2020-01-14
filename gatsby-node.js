556d31ecc903ed1670339fe1f0e9f4f9c61aa9ff
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2020-01-14 23:09:37 +0100

    someone please make it look good.

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,7 +25,7 @@ exports.createPages = async ({ actions: { createPage } }) => {
     var today = new Date();
     today.setHours(0,0,0,0);
     nextYear = new Date();
-    nextYear.setFullYear(nextYear.getFullYear() + 1)
+    nextYear.setDate(nextYear.getDate()+14);
     var response = await api.events.list({calendarId : calendarId, singleEvents: true, timeMin: today.toISOString(), timeMax: nextYear.toISOString(), maxResults: 1000 });
     var events = response.data.items.sort((a,b) => Date.parse(a.start.dateTime) - Date.parse(b.start.dateTime));
     console.log(response.data.items.length);
--- src/templates/events.js
+++ src/templates/events.js
@@ -1,12 +1,64 @@
-import React from 'react';
-
+import React from "react"
+import logo from "../images/logo.jpg" 
 export default ({ pageContext: { events } }) => (
   <div>
-    <h1>Test</h1>
-    <ul>
-      {events.map(x => (
-        <li>{x.start.dateTime} - {x.summary} - {x.description}</li>
-      ))}
-    </ul>
+    <div class="header">
+      <div class ="header2">
+        <img src={logo} class="logo" />
+      </div>
+    </div>
+    <div class="somemargin"></div>
+    {events.map(x => {
+      if(x.summary.toLowerCase().includes("laya")){
+       return (<div class="bgimg-3 day scroll">
+        <div class="caption">
+          <span class="border">
+            {x.start.dateTime} - {x.summary} - {x.description}
+          </span>
+        </div>
+     </div>);
+    }
+    else{
+      return (<div class="bgimg-2 day scroll">
+        <div class="caption">
+          <span class="border">
+            {x.start.dateTime} - {x.summary} - {x.description}
+          </span>
+        </div>
+     </div>);
+    }
+  })}
+    
+    {/* <div style="position:relative;">
+      <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: center;">
+        <p class="icons large-icons">
+          <a
+            href="http://www.facebook.com/rkmacharyacenter"
+            target="_blank"
+            class="icon icon-social-facebook"
+          ></a>
+          <a
+            href="https://www.instagram.com/rkm.acharyacenter/"
+            target="_blank"
+            class="icon icon-social-instagram"
+          ></a>
+          <a
+            href="https://www.linkedin.com/in/erika-erlando-16b82475/"
+            target="_blank"
+            class="icon icon-social-linkedin"
+          ></a>
+          <a
+            href="https://twitter.com/rkm_acharya"
+            target="_blank"
+            class="icon icon-social-twitter"
+          ></a>
+          <a
+            href="mailto:info@rkmacharyacenter.com"
+            class="icon icon-envelope"
+          ></a>
+        </p>
+      </div>
+    </div> */}
+    
   </div>
 )
