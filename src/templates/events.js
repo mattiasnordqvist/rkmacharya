c59705f8b315ec8ee62bd972385a206287b13bad
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2020-01-19 01:40:06 +0100

    some kind of POC of tag filtering

--- src/templates/events.js
+++ src/templates/events.js
@@ -1,73 +1,64 @@
 import React from "react"
-import logo from "../images/logo.jpg" 
 
 const days = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
 
-export default ({ pageContext: { events } }) => (
-  <div>
-    <div className="header">
-      <div className="header2">
-        <img src={logo} className="logo" />
-      </div>
-    </div>
-    <div className="somemargin"></div>
-    {events.map(x => {
-      if(x.summary.toLowerCase().includes("laya")){
-       return (<div className="bgimg-3 day scroll">
-        <div className="caption">
-          <span className="border">
-            <h1>{x.summary} - <a href={`https://maps.google.com/?q=${x.address}`}>{x.client} {x.location}</a></h1>
-            {x.cancelled && <h1>Cancelled :(</h1>}
-            <h2>{x.teacher}</h2>
-            <p>{days[new Date(x.start).getDay()]} {new Date(x.start).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})} - {new Date(x.end).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})}</p>
-          </span>
-        </div>
-     </div>);
+function Event({event}){
+    return (<div>
+        <h3>{event.summary} - <a href={`https://maps.google.com/?q=${event.address}`}>{event.client} {event.location}</a></h3>
+        {event.cancelled && <h4>Cancelled :(</h4>}
+        <h4>{event.teacher}</h4>
+        <p>{days[new Date(event.start).getDay()]} {new Date(event.start).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})} - {new Date(event.end).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})}</p>
+    </div>);
     }
-    else{
-      return (<div className="bgimg-2 day scroll">
-        <div className="caption">
-          <span className="border">
-            <h1>{x.summary} - <a href={`https://maps.google.com/?q=${x.address}`}>{x.client} {x.location}</a></h1>
-            {x.cancelled && <h1>Cancelled :(</h1>}
-            <h2>{x.teacher}</h2>
-            <p>{days[new Date(x.start).getDay()]} {new Date(x.start).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})} - {new Date(x.end).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})}</p>
-          </span>
-        </div>
-     </div>);
-    }
-  })}
+
+export default class Events extends React.Component
+{
+  constructor(props) {
     
-    {/* <div style="position:relative;">
-      <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: center;">
-        <p class="icons large-icons">
-          <a
-            href="http://www.facebook.com/rkmacharyacenter"
-            target="_blank"
-            class="icon icon-social-facebook"
-          ></a>
-          <a
-            href="https://www.instagram.com/rkm.acharyacenter/"
-            target="_blank"
-            class="icon icon-social-instagram"
-          ></a>
-          <a
-            href="https://www.linkedin.com/in/erika-erlando-16b82475/"
-            target="_blank"
-            class="icon icon-social-linkedin"
-          ></a>
-          <a
-            href="https://twitter.com/rkm_acharya"
-            target="_blank"
-            class="icon icon-social-twitter"
-          ></a>
-          <a
-            href="mailto:info@rkmacharyacenter.com"
-            class="icon icon-envelope"
-          ></a>
-        </p>
-      </div>
-    </div> */}
-    
-  </div>
-)
+    super(props);
+    this.state = { filter: {
+      teacher : {'Alice Henry': true, 'Erika Erlando': true },
+      summary : {'Layayoga': true, 'Acharyayoga': true, 'Layayoga Seniors': true}
+    }, events: props.pageContext.events };
+  }
+
+  filter = (e, where, what) => {
+    var filter = {...this.state.filter}
+    filter[where][what] = !filter[where][what];
+
+    var events = this.props.pageContext.events;
+    var newEvents = [];
+    events.forEach(e => {
+      if(filter.teacher[e.teacher]){
+        newEvents.push(e);
+      }
+    });
+    events = newEvents;
+
+    var newEvents = [];
+    events.forEach(e => {
+      if(filter.summary[e.summary]){
+        newEvents.push(e);
+      }
+    });
+    events = newEvents;
+
+    this.setState({filter, events: newEvents});
+  }
+
+  render()
+  {
+    return (
+      <div>
+        {Object.keys(this.state.filter.teacher).map((k) => {
+           return <button type="button" onClick={(e) => this.filter(e,'teacher',k)}key={k}>{k} - {this.state.filter.teacher[k] ? 'yes':'no'}</button>
+        })}
+         {Object.keys(this.state.filter.summary).map((k) => {
+           return <button type="button" onClick={(e) => this.filter(e,'summary',k)}key={k}>{k} - {this.state.filter.summary[k] ? 'yes':'no'}</button>
+        })}
+        {this.state.events.map((e) => {
+          return <Event key={e.location+e.start} event={e}></Event>
+        })}
+      </div>);
+  }
+}
