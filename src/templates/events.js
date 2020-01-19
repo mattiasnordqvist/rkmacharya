97153997134d96fbe4b3016ab3829b9667914ca5
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2020-01-19 13:56:19 +0100

    added day filter

--- src/templates/events.js
+++ src/templates/events.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-const days = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
+const days = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
 
 const uniques = function(arr) {
   var a = [];
   for (var i=0, l=arr.length; i<l; i++)
-      if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
-          a.push(arr[i]);
+  {
+    if (a.indexOf(arr[i]) === -1 && arr[i] !== '')
+    { 
+      a.push(arr[i]);
+    }
+  }
   return a;
 }
 
@@ -15,7 +19,7 @@ function Event({event}){
         <h3>{event.summary} - <a href={`https://maps.google.com/?q=${event.address}`}>{event.client} {event.location}</a></h3>
         {event.cancelled && <h4>Cancelled :(</h4>}
         <h4>{event.teacher}</h4>
-        <p>{days[new Date(event.start).getDay()]} {new Date(event.start).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})} - {new Date(event.end).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})}</p>
+        <p>{event.day} {new Date(event.start).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})} - {new Date(event.end).toLocaleTimeString('sv-SE', {hour: '2-digit', minute:'2-digit', hour12: false})}</p>
     </div>);
     }
 
@@ -25,17 +29,32 @@ export default class Events extends React.Component
     super(props);
     var teacherFilter = uniques(props.pageContext.events.map(x => x.teacher)).reduce((x,y) => {x[y] = true; return x;}, {});
     var summaryFilter = uniques(props.pageContext.events.map(x => x.summary)).reduce((x,y) => {x[y] = true; return x;}, {});
-    this.state = { filter: {
-      teacher: teacherFilter,
-      summary: summaryFilter
-    }, events: props.pageContext.events };
+    var dayFilter = days.reduce((x,y) => {x[y] = true; return x;}, {});
+    this.state = { 
+        filter: {
+          teacher: teacherFilter,
+          summary: summaryFilter,
+          day: dayFilter,
+        },
+        events: props.pageContext.events 
+      };
   }
 
-  filter = (e, where, what) => {
+  componentDidMount() {
+    this.filterEvents()
+  }
+
+  toggleFilter = (e, where, what) => {
     var filter = {...this.state.filter}
     filter[where][what] = !filter[where][what];
+    this.setState({...this.state, filter});
+    this.filterEvents();
+  }
 
-    var events = this.props.pageContext.events;
+  appendData = (events) => events.map(x => ({...x, day: days[(new Date(x.start).getDay()+6) % 7] }));
+
+  filterEvents = () => {
+    var events = this.appendData(this.props.pageContext.events);
     var newEvents = [];
 
     Object.keys(this.state.filter).forEach(f => {
@@ -48,21 +67,28 @@ export default class Events extends React.Component
       newEvents = [];  
     });
 
-    this.setState({filter, events});
+    this.setState({...this.state, events});
   }
 
   render()
   {
+    console.log(this.state);
+    if(this.state && this.state.filter && this.state.events){
     return (
       <div>
         {Object.keys(this.state.filter).map((f) => 
          Object.keys(this.state.filter[f]).map((k) => 
-          <button type="button" onClick={(e) => this.filter(e,f,k)}key={k}>{k} - {this.state.filter[f][k] ? 'yes':'no'}</button>
+          <button type="button" onClick={(e) => this.toggleFilter(e,f,k)}key={k}>{k} - {this.state.filter[f][k] ? 'yes':'no'}</button>
         ))}
        
         {this.state.events.map((e) => 
           <Event key={e.location+e.start} event={e}></Event>
         )}
-      </div>);
+      </div>)
+      }
+      else
+      {
+        return (<div>Loading...</div>)
+      };
   }
 }
