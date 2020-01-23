b92644a95231cc54100e3d3a56b326ad20f536e5
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2020-01-23 23:07:51 +0100

    filter-component

--- src/templates/events.js
+++ src/templates/events.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -49,13 +49,24 @@ function Event({ event }) {
   )
 }
 
+const ToggleFilter = ({values}) => {
+  const [toggles, setToggles] = useState(values.reduce((x, y) => { x[y] = true; return x; }, {}));
+  return (<div>
+    {values.map(v => 
+    <button type="button" key={v} onClick={() => setToggles(Object.assign({}, toggles, (function(attr, val){ var a = {}; a[attr]=val; return a; })(v,!toggles[v])))}> 
+      {v} - {toggles[v] ? "yes" : "no"}
+    </button>)}
+  </div>);
+}
+
 export default class Events extends React.Component {
   constructor(props) {
     super(props)
-    var teacherFilter = uniques(props.pageContext.events.map(x => x.teacher)).reduce((x, y) => { x[y] = true; return x; }, {});
-    var summaryFilter = uniques(props.pageContext.events.map(x => x.summary)).reduce((x, y) => { x[y] = true; return x; }, {});
-    var locationFilter = uniques(props.pageContext.events.map(x => x.client + " " + x.location)).reduce((x, y) => { x[y] = true; return x; }, {});
-    var dayFilter = days.reduce((x, y) => { x[y] = true; return x }, {})
+    
+    // var teacherFilter = uniques(props.pageContext.events.map(x => x.teacher)).reduce((x, y) => { x[y] = true; return x; }, {});
+    // var summaryFilter = uniques(props.pageContext.events.map(x => x.summary)).reduce((x, y) => { x[y] = true; return x; }, {});
+    // var locationFilter = uniques(props.pageContext.events.map(x => x.client + " " + x.location)).reduce((x, y) => { x[y] = true; return x; }, {});
+    // var dayFilter = days.reduce((x, y) => { x[y] = true; return x }, {})
     var dateFilter = {
       check: (e) => e.start >= this.dates[this.active],
       dayNames: [0,1,2,3,4,5,6].map(x=>days[(x+todayIndex)%7]), 
@@ -65,44 +76,48 @@ export default class Events extends React.Component {
         return someDay;
       }),
       active: 0};
-    var filter = {
-      teacher: teacherFilter,
-      summary: summaryFilter,
-      day: dayFilter,
-      clientAndLocation: locationFilter,
-    }
+    // var filter = {
+    //   teacher: teacherFilter,
+    //   summary: summaryFilter,
+    //   day: dayFilter,
+    //   clientAndLocation: locationFilter,
+    // }
     this.state = {
-      filter,
+      teachers: uniques(props.pageContext.events.map(x => x.teacher)),
+      summaries: uniques(props.pageContext.events.map(x => x.summary)),
+      days: days,
+      clientsAndLocations: uniques(props.pageContext.events.map(x => x.client + " " + x.location)),
+      // filter,
       events: this.appendData(props.pageContext.events)
     }
   }
 
-  componentDidMount() {
-    if(localStorage.getItem("filter"))
-    {
-      var newFilter = this.state.filter;
-      var oldFilter = JSON.parse(localStorage.getItem("filter"));
-      Object.keys(newFilter).forEach(x => 
-      {
-        Object.keys(newFilter[x]).forEach(y => {
-          if(oldFilter.hasOwnProperty(x) && newFilter[x].hasOwnProperty(y)){
-            newFilter[x][y] = oldFilter[x][y];
-          }
-        })
-      });
-      localStorage.setItem("filter", JSON.stringify(newFilter));
-      this.setState({filter:newFilter});
-    }
-    this.filterEvents();
-  }
+  // componentDidMount() {
+  //   if(localStorage.getItem("filter"))
+  //   {
+  //     var newFilter = this.state.filter;
+  //     var oldFilter = JSON.parse(localStorage.getItem("filter"));
+  //     Object.keys(newFilter).forEach(x => 
+  //     {
+  //       Object.keys(newFilter[x]).forEach(y => {
+  //         if(oldFilter.hasOwnProperty(x) && newFilter[x].hasOwnProperty(y)){
+  //           newFilter[x][y] = oldFilter[x][y];
+  //         }
+  //       })
+  //     });
+  //     localStorage.setItem("filter", JSON.stringify(newFilter));
+  //     this.setState({filter:newFilter});
+  //   }
+  //   this.filterEvents();
+  // }
 
-  toggleFilter = (e, where, what) => {
-    var filter = { ...this.state.filter }
-    filter[where][what] = !filter[where][what];
-    localStorage.setItem('filter', JSON.stringify(filter));
-    this.setState({ ...this.state, filter });
-    this.filterEvents();
-  }
+  // toggleFilter = (e, where, what) => {
+  //   var filter = { ...this.state.filter }
+  //   filter[where][what] = !filter[where][what];
+  //   localStorage.setItem('filter', JSON.stringify(filter));
+  //   this.setState({ ...this.state, filter });
+  //   this.filterEvents();
+  // }
 
   appendData = events =>
     events.map(x => ({
@@ -112,28 +127,32 @@ export default class Events extends React.Component {
       visible: true
     }))
 
-  filterEvents = () => {
-    var events = this.state.events;
-    events.forEach(e => {
-      e.visible = true;
-    });
-    Object.keys(this.state.filter).forEach(f => {
-      events.forEach(e => {
-        if (!this.state.filter[f][e[f]]) {
-          e.visible &= false;
-        }
-      })
-    });
+  // filterEvents = () => {
+  //   var events = this.state.events;
+  //   events.forEach(e => {
+  //     e.visible = true;
+  //   });
+  //   Object.keys(this.state.filter).forEach(f => {
+  //     events.forEach(e => {
+  //       if (!this.state.filter[f][e[f]]) {
+  //         e.visible &= false;
+  //       }
+  //     })
+  //   });
 
-    this.setState({ ...this.state, events })
-  }
+  //   this.setState({ ...this.state, events })
+  // }
 
   render() {
-    if (this.state && this.state.filter && this.state.events) {
+    if (this.state && this.state.events) {
       return (
         <Layout>
           <SEO title="Home"></SEO>
-          {Object.keys(this.state.filter).map(f =>
+          <ToggleFilter values={this.state.teachers}></ToggleFilter>
+          <ToggleFilter values={this.state.summaries}></ToggleFilter>
+          <ToggleFilter values={this.state.days}></ToggleFilter>
+          <ToggleFilter values={this.state.clientsAndLocations}></ToggleFilter>
+          {/* {Object.keys(this.state.filter).map(f =>
             Object.keys(this.state.filter[f]).map(k => (
               <button
                 type="button"
@@ -143,7 +162,7 @@ export default class Events extends React.Component {
                 {k} - {this.state.filter[f][k] ? "yes" : "no"}
               </button>
             ))
-          )}
+          )} */}
 
           {this.state.events.map(e => (
             <Event key={e.location + e.start} event={e}></Event>
